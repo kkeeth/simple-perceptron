8943e6f49bc87283aefa5bae817314b7db59f787
kuwahara <kuwahara@lepra.jp>
2016-01-30 19:03:49 +0900

    update test case

--- test/simple_perceptron_test.php
+++ test/simple_perceptron_test.php
@@ -5,17 +5,17 @@ class SimplePerceptronTest extends PHPUnit_Framework_TestCase {
 
    public function test_index() {
       // 正常系
-      list($ret, $val) = calc_recognition([1,1,-1], [2, 0, 1]);
+      list($ret, $val) = Calc_recognition([1,1,-1], [2, 0, 1], 1);
       $this->assertEquals(1, $ret);
       $this->assertEquals(1, $val);
 
       // 異常系
       // 個数エラー
-      list($ret, $val) = calc_recognition([1,1,-1], [2, 0]);
+      list($ret, $val) = Calc_recognition([1,1,-1], [2, 0], 1);
       $this->assertEquals(-1, $val);
 
       // nullエラー
-      list($ret, $val) = calc_recognition([1,1,-1]);
+      list($ret, $val) = Calc_recognition([1,1,-1], [], 0);
       $this->assertEquals(-1, $val);
 
    }
