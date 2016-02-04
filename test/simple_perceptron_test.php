7194e26803432a39fd36e2af6f5e80c6d1a19bd7
k-kuwahara <kuwahara@lepra.jp>
2016-02-04 11:04:16 +0900

    update doc

--- test/simple_perceptron_test.php
+++ test/simple_perceptron_test.php
@@ -3,20 +3,19 @@ require_once "../src/simple_perceptron.php";
 
 class SimplePerceptronTest extends PHPUnit_Framework_TestCase
 {
-
    public function test_index()
    {
-      // 正常系
+      // normal
       list($ret, $val) = Calc_recognition([1,1,-1], [2, 0, 1], 1);
       $this->assertEquals(1, $ret);
       $this->assertEquals(1, $val);
 
-      // 異常系
-      // 個数エラー
+      // error
+      // data count error
       list($ret, $val) = Calc_recognition([1,1,-1], [2, 0], 1);
       $this->assertEquals(-1, $val);
 
-      // nullエラー
+      // null error
       list($ret, $val) = Calc_recognition([1,1,-1], [], 0);
       $this->assertEquals(-1, $val);
 
