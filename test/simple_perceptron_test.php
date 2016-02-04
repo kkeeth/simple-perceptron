60aa6d4ea58f8a4771070dd0d1557a5ef4897c1e
k-kuwahara <kuwahara@lepra.jp>
2016-02-04 16:33:46 +0900

    add unit test cases

--- test/simple_perceptron_test.php
+++ test/simple_perceptron_test.php
@@ -3,20 +3,94 @@ require_once "../src/simple_perceptron.php";
 
 class SimplePerceptronTest extends PHPUnit_Framework_TestCase
 {
-   public function test_index()
+   /**
+    * Preparation of a test data
+    *
+    */
+   public function setUp()
+   {
+      $this->obj = new simple_perceptron();
+      $this->normal_input = [
+         [5, 2, 3],
+         [4, 4, 5],
+         [1, 2, 4],
+         [4, 1, 1],
+         [2, 1, 5],
+     ];
+
+      $this->element_error_input = [
+         [5, 2, 3],
+         [4, 4, 5],
+         [1, 2, 4],
+         [4, 1],
+         [2, 1, 5],
+
+      ];
+
+      $this->count_error_input = [
+         [5, 2, 3],
+         [4, 4, 5],
+         [],
+         [4, 1, 1],
+         [2, 1, 5],
+
+      ];
+
+      $this->duplicate_error_input = [
+         [1, 2, 3],
+         [4, 4, 5],
+         [1, 2, 4],
+         [4, 1, 1],
+         [2, 1, 5],
+    ];
+
+      $this->label = [1, 1, -1, -1, 1];
+   }
+
+   /**
+    * Create test data
+    *
+    */
+   public function test_Create_data()
+   {
+      list($data, $labels) = $this->obj->Create_data();
+      $this->assertCount(5, $data);
+      $this->assertCount(3, $data[0]);
+      $this->assertCount(5, $labels);
+   }
+
+   /**
+    * Add parameter 1 for input data
+    *
+    */
+   public function test_Get_data()
+   {
+      $data = $this->obj->Get_data($this->normal_input[0]);
+      $this->assertCount(4, $data);
+      $this->assertEquals(1, array_pop($data));
+   }
+
+   /**
+    * Execute test
+    *
+    */
+   public function test_Calc_recognition()
    {
       // normal
-      list($ret, $val) = Calc_recognition([1,1,-1], [2, 0, 1], 1);
-      $this->assertEquals(1, $ret);
-      $this->assertEquals(1, $val);
+      $ret = [];
+      $ret = $this->obj->Calc_recognition('', $this->normal_input, $this->label);
+      $this->assertTrue(is_array($ret));
+
+      // element count error
+      $ret = $this->obj->Calc_recognition('', $this->element_error_input, $this->label);
+      $this->assertFalse($ret);
 
       // data count error
-      list($ret, $val) = Calc_recognition([1,1,-1], [2, 0], 1);
-      $this->assertEquals(-1, $val);
+      $ret = $this->obj->Calc_recognition('', $this->count_error_input, $this->label);
+      $this->assertFalse($ret);
 
-      // null error
-      list($ret, $val) = Calc_recognition([1,1,-1], [], 0);
-      $this->assertEquals(-1, $val);
-
+      // data duplicate error
+      $ret = $this->obj->Calc_recognition('', $this->duplicate_error_input, $this->label);
+      $this->assertFalse($ret);
    }
 }
